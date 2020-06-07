41cc87045b98a2ff2011ed4210841e9506aad611
Steven Yau <social@stevenyau.co.uk>
2020-06-08 00:09:42 +0100

    Added physics debugger

--- playcanvas-devtools/injector.js
+++ playcanvas-devtools/injector.js
@@ -11,15 +11,36 @@ if (!__addedDebugTools__) {
             }
             
             var scriptFilenames = [
+                'dat.gui.min.js',
                 'playcanvas-extras.js',
-                'dat.gui.min.js'
+                'debug-physics.js'
             ];
+
+            var app = pc.Application.getApplication();
+            var debugEntityName = '__devtools__';
+
+            var addScriptTypeToDebugEntity = function (scriptName, data) {
+                var debugEntity = app.root.findByName(debugEntityName);
+                if (!debugEntity) {
+                    debugEntity = new pc.Entity();
+                    debugEntity.addComponent('script');
+                    app.root.addChild(debugEntity);
+                }
+
+                var scriptInstance = debugEntity.script[scriptName];
+                if (!scriptInstance) {
+                    scriptInstance = debugEntity.script.create(scriptName, {
+                        attributes: data
+                    });
+                }
+
+                return scriptInstance;
+            }
 
             var callback = function () {
                 console.log('All PlayCanvas Debug Tool scripts loaded');
 
                 // Load the ministats
-                var app = pc.Application.getApplication();
                 var ministats = new pc.MiniStats(app);
 
                 // Load dat gui
@@ -27,6 +48,17 @@ if (!__addedDebugTools__) {
                 var ministatsFolder = datgui.addFolder('Mini Stats');
                 ministatsFolder.add(ministats, 'enabled');
 
+                // Add the physics debugger
+                var debugPhysics = addScriptTypeToDebugEntity('debugPhysics', {
+                    drawShapes: false,
+                    opacity: 0.5,
+                    castShadows: false              
+                });
+
+                var debugPhysicsFolder = datgui.addFolder('Physics');
+                debugPhysicsFolder.add(debugPhysics, 'drawShapes');
+                debugPhysicsFolder.add(debugPhysics, 'opacity', 0, 1);
+                debugPhysicsFolder.add(debugPhysics, 'castShadows');
             };
 
             var scriptsLoaded = 0;
--- playcanvas-devtools/readme.md
+++ playcanvas-devtools/readme.md
@@ -4,3 +4,8 @@ Create a bookmarklet by making a new bookmark and changing URL to the following:
 ```
 javascript:(function(){var a=document.createElement('script');a.src='https://yaustar.github.io/playcanvas-devtools/injector.js';document.head.appendChild(a);})();
 ```
+
+# Tools
+
+* Will's Debug physics renderer
+* Donovan's ministats
