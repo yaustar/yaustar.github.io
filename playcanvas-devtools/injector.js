b1c8e8d060d0c9785f94786b09718a49125c39e5
Steven Yau <social@stevenyau.co.uk>
2020-07-06 00:22:55 +0100

    Started reveal.js presentation for PlayCanvas for Tentacle Zone

--- playcanvas-devtools/injector.js
+++ playcanvas-devtools/injector.js
@@ -3,7 +3,7 @@ if (!__addedDebugTools__) {
     (function () {
         if (!__addedDebugTools__) {
             var baseUrl;
-            var useGitHubUrl = true;
+            var useGitHubUrl = false;
             if (useGitHubUrl) {
                 baseUrl = 'https://yaustar.github.io/playcanvas-devtools/';
             } else {
