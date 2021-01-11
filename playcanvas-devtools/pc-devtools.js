346f72b7c947a2a7ef7f5698a465885734b49315
Steven <yaustar@users.noreply.github.com>
2021-01-11 18:37:40 +0000

    Add object watch shim

--- playcanvas-devtools/pc-devtools.js
+++ playcanvas-devtools/pc-devtools.js
@@ -152,6 +152,51 @@
 
         return scriptInstance;
     };
+    
+    // Add a object watch shim
+    if (!Object.prototype.watch) {
+        Object.defineProperty(Object.prototype, "watch", {
+              enumerable: false
+            , configurable: true
+            , writable: false
+            , value: function (prop, handler) {
+                var
+                  oldval = this[prop]
+                , newval = oldval
+                , getter = function () {
+                    return newval;
+                }
+                , setter = function (val) {
+                    oldval = newval;
+                    return newval = handler.call(this, prop, oldval, val);
+                }
+                ;
+
+                if (delete this[prop]) { // can't watch constants
+                    Object.defineProperty(this, prop, {
+                          get: getter
+                        , set: setter
+                        , enumerable: true
+                        , configurable: true
+                    });
+                }
+            }
+        });
+    }
+
+    // object.unwatch
+    if (!Object.prototype.unwatch) {
+        Object.defineProperty(Object.prototype, "unwatch", {
+              enumerable: false
+            , configurable: true
+            , writable: false
+            , value: function (prop) {
+                var val = this[prop];
+                delete this[prop]; // remove accessors
+                this[prop] = val;
+            }
+        });
+    }
 
     window.pcDevtools = pcDevtools;
 })();
