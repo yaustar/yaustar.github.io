ed6006fb0d81d4a0be5ab7f03b54f261832505f9
Steven <yaustar@users.noreply.github.com>
2021-01-12 14:58:04 +0000

    Update __settings__.js

--- flappy/__settings__.js
+++ flappy/__settings__.js
@@ -9,7 +9,7 @@ CONTEXT_OPTIONS = {
     'powerPreference': "default"
 };
 SCRIPTS = [ 37175323, 21399876, 4554207, 4554213, 4554214, 4554217, 4554270, 4554271, 4554273, 4554276, 4554279, 20754603, 20755574, 37175329, 37186400 ];
-CONFIG_FILENAME = "config.json";
+CONFIG_FILENAME = "https://yaustar.github.io/flappy/config.json";
 INPUT_SETTINGS = {
     useKeyboard: true,
     useMouse: true,
