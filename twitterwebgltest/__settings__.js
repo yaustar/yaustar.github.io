af52a89e9441bd4b6c5f58a0b4eaa3c75cd8e6d3
Steven Yau <syau@snapchat.com>
2022-01-06 18:26:15 +0000

    Twitter embedding test

---
+++ twitterwebgltest/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1280790.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 61365365, 61365364, 61365377, 61365378, 61367326, 61367327 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
