3e190318e5e5eb68618e8a48fa96b81d88ead428
Steven Yau <social@stevenyau.co.uk>
2022-07-23 14:36:05 +0100

    Added load glb options WIP

--- playcanvas-editor-api-tools/editor-utils.user.js
+++ playcanvas-editor-api-tools/editor-utils.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         PlayCanvas Editor tools
-// @namespace    http://tampermonkey.net/
+// @name         PlayCanvas Editor Uber Tools
+// @namespace    http://stevenyau.co.uk/
 // @version      0.1
-// @description  yaustar's PlayCanvas Editor Tools
+// @description  yaustar's PlayCanvas Editor Uber Tools
 // @author       @yaustar
 // @match        https://playcanvas.com/editor/scene/*
 // @icon         https://www.google.com/s2/favicons?domain=playcanvas.com
@@ -10,17 +10,113 @@
 // ==/UserScript==
 
 
-(function() {
+(function () {
     'use strict';
     const logCssStyle = 'color: white; background-color: #8e44ad';
-    const onEngineLoaded = function() {
+    const onEngineLoaded = function () {
         console.log("%c yaustar's tools loaded :) ", logCssStyle);
 
         const app = pc.Application.getApplication();
         let menu = null;
         const root = editor.call('layout.root');
 
-        app.mouse.on('mousedown', function(e) {
+        const glbEntityName = '8556102755';
+
+        const loadGlbContainerFromAsset = function (glbBinAsset, options, assetName, callback) {
+            var onAssetReady = function (asset) {
+                var blob = new Blob([asset.resource]);
+                var data = URL.createObjectURL(blob);
+                return loadGlbContainerFromUrl(data, options, assetName, function (error, asset) {
+                    callback(error, asset);
+                    URL.revokeObjectURL(data);
+                });
+            }.bind(this);
+
+            glbBinAsset.ready(onAssetReady);
+            app.assets.load(glbBinAsset);
+        };
+
+        const loadGlbContainerFromUrl = function (url, options, assetName, callback) {
+            var filename = assetName + '.glb';
+            var file = {
+                url: url,
+                filename: filename
+            };
+
+            var asset = new pc.Asset(filename, 'container', file, null, options);
+            asset.once('load', function (containerAsset) {
+                if (callback) {
+                    callback(null, containerAsset);
+                }
+            });
+
+            app.assets.add(asset);
+            app.assets.load(asset);
+
+            return asset;
+        };
+
+        const createGlbEntity = function (editorEntity) {
+            const scripts = editorEntity.get('components.script.scripts');
+            if (scripts && (scripts.loadGlbAsset || scripts.loadGlbUrl)) {
+                const guid = editorEntity.get('resource_id');
+                const viewEntity = app.root.findByGuid(guid);
+
+                // Check if a GLB entity has already been created 
+                let alreadyLoaded = false;
+                const viewEntityChildren = viewEntity.children;
+                for (let i = 0; i < viewEntityChildren.length; ++i) {
+                    if (viewEntityChildren[i].name === glbEntityName) {
+                        alreadyLoaded = true;
+                    }
+                }
+
+                if (alreadyLoaded) {
+                    console.log('Already loaded a GLB');
+                } else {
+                    if (scripts.loadGlbAsset) {
+                        const assetId = scripts.loadGlbAsset.attributes.glbAsset;
+                        if (assetId) {
+                            // Load the asset from the registry
+                            const asset = app.assets.get(assetId);
+                            loadGlbContainerFromAsset(asset, null, asset.name, function (err, asset) {
+                                if (err) {
+                                    console.error(err);
+                                    return;
+                                }
+
+                                const renderRootViewEntity = asset.resource.instantiateRenderEntity();
+                                renderRootViewEntity.name = glbEntityName;
+                                renderRootViewEntity.reparent(viewEntity);
+
+                                console.log('Loaded GLB: ' + asset.name);
+                            });
+                        }
+                    }
+
+                    if (scripts.loadGlbUrl) {
+                        const url = scripts.loadGlbUrl.attributes.glbUrl;
+                        if (url) {
+                            const filename = url.substring(url.lastIndexOf('/')+1);
+                            loadGlbContainerFromUrl(url, null, filename, function (err, asset) {
+                                if (err) {
+                                    console.error(err);
+                                    return;
+                                }
+
+                                const renderRootViewEntity = asset.resource.instantiateRenderEntity();
+                                renderRootViewEntity.name = glbEntityName;
+                                renderRootViewEntity.reparent(viewEntity);
+
+                                console.log('Loaded GLB: ' + asset.name);
+                            });
+                        }
+                    }
+                }
+            }
+        }
+
+        app.mouse.on('mousedown', function (e) {
             if (e.button === pc.MOUSEBUTTON_RIGHT) {
                 if (menu) {
                     root.remove(menu);
@@ -41,7 +137,7 @@
                         const entryTitle = menuIndex.toString() + ': ' + entityName;
                         menuItems.push({
                             text: entryTitle,
-                            onSelect: function() {
+                            onSelect: function () {
                                 editor.selection.set([entityTarget]);
                             }
                         });
@@ -49,6 +145,13 @@
                         parent = parent.parent;
                         menuIndex += 1;
                     }
+
+                    menuItems.push({
+                        text: 'Load GLB',
+                        onSelect: function() {
+                            createGlbEntity(selectedEntity)
+                        }
+                    });
 
                     const menuArgs = {
                         items: menuItems,
@@ -72,7 +175,7 @@
 
     // Wait for the PlayCanvas application to be loaded
 
-    const intervalId = setInterval(function() {
+    const intervalId = setInterval(function () {
         if (pc.Application.getApplication()) {
             onEngineLoaded();
             clearInterval(intervalId);
