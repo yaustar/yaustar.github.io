b4abd4d7df1b9d363f1753d244baaaa7c875bc48
Steven Yau <syau@snapchat.com>
2023-03-27 17:54:57 +0100

    test 16.3.1

---
+++ test-16.3.1-vConsole/__settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1698289.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 126494368, 126494347, 126494350, 126494367, 126494346, 126494378, 126494382 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
---
+++ test-16.3.1-vConsole/files/assets/126494382/1/vconsole-integration.js
@@ -0,0 +1,14 @@
+(function(){
+    window.vConsole = new VConsole();
+    setTimeout(()=> {
+        window.vConsole.show();
+    },250);
+
+    setTimeout(()=> {
+        window.vConsole.show();
+    },500);
+
+    setTimeout(()=> {
+        window.vConsole.show();
+    },1000);
+})();
